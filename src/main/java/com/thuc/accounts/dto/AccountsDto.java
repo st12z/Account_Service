850e75cd30c2293a4e1ea7bc867cc540a3b665f2
st12z <ckp2004vn@gmail.com>
2025-02-27 22:52:40 +0700

    valid input AccountsDto and CustomerDtos

--- src/main/java/com/thuc/accounts/dto/AccountsDto.java
+++ src/main/java/com/thuc/accounts/dto/AccountsDto.java
@@ -1,5 +1,7 @@
 package com.thuc.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class AccountsDto {
+    @NotEmpty(message = "Account number can not be null or empty")
+    @Pattern(regexp = "^$|[0-9]{10}",message = "AccountNumber must be 10 digits")
     private Long accountNumber;
+    @NotEmpty(message = "Account type can not be null or empty")
     private String accountType;
+    @NotEmpty(message = "Branch Address type can not be null or empty")
     private String branchAddress;
 }
--- src/main/java/com/thuc/accounts/dto/CustomerDto.java
+++ src/main/java/com/thuc/accounts/dto/CustomerDto.java
@@ -1,5 +1,9 @@
 package com.thuc.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +14,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CustomerDto {
+    @NotEmpty(message = "Name can not be a null or empty")
+    @Size(min = 5,max = 30,message = "The length ò the customer name should be 5-30 ")
     private String name;
+    @NotEmpty(message = "Email can not be a null or empty")
+    @Email(message = "Email is invalid")
     private String email;
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
     private String mobileNumber;
     private AccountsDto accountsDto;
 }
